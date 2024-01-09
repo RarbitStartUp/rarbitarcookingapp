226d98d0772d47dc720af1fbb7d7b1c5a4ef5b8f
RarbitStartUp <rarbitstartup@gmail.com>
2024-01-10 01:13:42 +0800

    responseive

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,23 +1,54 @@
+"use client";
+
 import NavLink from "./navLink/navLink";
 import styles from "./links.module.css";
+import { useState } from "react";
 
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Upload Video",
+    path: "/uploadVideo",
+  },
+];
 const Links = () => {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "Upload Video",
-      path: "/uploadVideo",
-    },
-  ];
+  const [open, setOpen] = useState(false);
+
+  //TEMPORARY
+  // const session = true;
+  // const isAdmin = true;
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {/* {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )} */}
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
