ca60ad032c6736b20d514d142ab9d95ca542420b
RarbitStartUp <rarbitstartup@gmail.com>
2024-01-21 17:22:45 +0800

    tmux

--- src/app/WebsocketProvider.jsx
+++ src/app/WebsocketProvider.jsx
@@ -5,7 +5,7 @@ import { createContext, useContext } from 'react';
 const WebSocketContext = createContext();
 
 export const WebSocketProvider = ({ children }) => {
-  const socket = new WebSocket("ws://localhost:3001");
+  const socket = new WebSocket("wss://6be9-89-187-185-185.ngrok-free.app");
 
   return (
     <WebSocketContext.Provider value={socket}>
