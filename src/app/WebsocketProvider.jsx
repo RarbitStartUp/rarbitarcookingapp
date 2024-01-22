02fe439c0729b4bd213debe55a12345a4af61b97
RarbitStartUp <rarbitstartup@gmail.com>
2024-01-22 18:01:02 +0800

    AWS ws

--- src/app/WebsocketProvider.jsx
+++ src/app/WebsocketProvider.jsx
@@ -5,7 +5,7 @@ import { createContext, useContext } from 'react';
 const WebSocketContext = createContext();
 
 export const WebSocketProvider = ({ children }) => {
-  const socket = new WebSocket("wss://6be9-89-187-185-185.ngrok-free.app");
+  const socket = new WebSocket("ws://18.222.93.182");
 
   return (
     <WebSocketContext.Provider value={socket}>
