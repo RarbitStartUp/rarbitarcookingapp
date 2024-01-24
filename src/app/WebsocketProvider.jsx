a02893eaac09e5516b3cf54d3c6316f127207bb6
RarbitStartUp <rarbitstartup@gmail.com>
2024-01-24 15:30:00 +0800

    socket.io

--- src/app/WebsocketProvider.jsx
+++ src/app/WebsocketProvider.jsx
@@ -7,7 +7,15 @@ const WebSocketContext = createContext();
 
 export const WebSocketProvider = ({ children }) => {
   // const socket = new WebSocket("ws://localhost:3001");
-  const socket = io("https://rarbit.tech");
+  const socket = io("https://rarbit.tech",
+    {
+      reconnection: true,
+      reconnectionAttempts: Infinity, // Try to reconnect indefinitely
+      reconnectionDelay: 1000, // in milliseconds
+      reconnectionDelayMax: 5000,
+      randomizationFactor: 0.5,
+      autoConnect: true, // Enable auto-connect on initialization
+  });
 
   return (
     <WebSocketContext.Provider value={socket}>
