38f2da372027897abc8c9c197a8b2a433350f78e
RarbitStartUp <rarbitstartup@gmail.com>
2024-02-11 23:41:27 +0800

    auth6

--- src/lib/getGoogleServiceAccountKey.js
+++ src/lib/getGoogleServiceAccountKey.js
@@ -1,13 +1,18 @@
 // lib/getGoogleServiceAccountKey.js
 
 import { SecretsManagerClient, GetSecretValueCommand } from "@aws-sdk/client-secrets-manager";
-import { fromIni } from "@aws-sdk/credential-provider-ini";
+// import { fromIni } from "@aws-sdk/credential-provider-ini";
 
 export async function getGoogleServiceAccountKey() {
   const secretName = "google_service_key.json";
   const client = new SecretsManagerClient({ 
     region: "us-east-2",
-    credentials: fromIni({}) });
+    // credentials: fromIni({}) 
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    }
+  });
   
   try {
     const response = await client.send(
