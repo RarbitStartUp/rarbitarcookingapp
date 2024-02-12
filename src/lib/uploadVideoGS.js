163351fe6fabda89db4b6281826bb2159db50a78
RarbitStartUp <rarbitstartup@gmail.com>
2024-02-12 19:51:01 +0800

    commit

--- src/lib/uploadVideoGS.js
+++ src/lib/uploadVideoGS.js
@@ -2,8 +2,8 @@
 import { Storage } from "@google-cloud/storage";
 import { checkboxAI } from "./checkboxAI";
 import ytdl from "ytdl-core";
-// import { getGCPCredentials } from "./getGCPCredentials";
-import { getGoogleServiceAccountKey } from "./getGoogleServiceAccountKey"
+import { getGCPCredentials } from "./getGCPCredentials";
+// import { getGoogleServiceAccountKey } from "./getGoogleServiceAccountKey"
 
 export async function uploadVideoGS(formData) {
   try {
@@ -13,24 +13,25 @@ export async function uploadVideoGS(formData) {
     const bucketName = "users_uploads";
     // const credentials = getGCPCredentials();
     // console.log("credentials:", credentials);
-    const secret = await getGoogleServiceAccountKey();
+    // const secret = await getGoogleServiceAccountKey();
     // console.log("secret:", secret);
 
     // Parse the secret JSON string
-    const { client_email, private_key } = JSON.parse(secret);
+    // const { client_email, private_key } = JSON.parse(secret);
 
     // Create a credentials object
-    const credentials = {
-       client_email,
-       private_key,
-    };
-    console.log("credentials:", credentials)
+    // const credentials = {
+    //    client_email,
+    //    private_key,
+    // };
+    // console.log("credentials:", credentials)
 
+    const storageClient = new Storage(getGCPCredentials());
     // const storageClient = new Storage(credentials);
-    const storageClient = new Storage({
-      projectId: "arcookingapp",
-      credentials: credentials
-     });
+    // const storageClient = new Storage({
+    //   projectId: "arcookingapp",
+    //   credentials: credentials
+    //  });
     console.log("storageClient:", storageClient);
 
     let totalFileSize = 0; // Initialize fileSize to zero
@@ -44,16 +45,16 @@ export async function uploadVideoGS(formData) {
 
     const videoStream = ytdl(inputLink, { filter: "audioandvideo" });
 
-    videoStream.on("progress", (_, totalBytes, totalBytesExpected) => {
-      totalBytesTransferred = totalBytes;
-      totalFileSize = totalBytesExpected;
+    // videoStream.on("progress", (_, totalBytes, totalBytesExpected) => {
+    //   totalBytesTransferred = totalBytes;
+    //   totalFileSize = totalBytesExpected;
 
-      const progress = (totalBytesTransferred / totalFileSize) * 100;
-      console.log("Progress:", progress.toFixed(2) + "%");
+    //   const progress = (totalBytesTransferred / totalFileSize) * 100;
+    //   console.log("Progress:", progress.toFixed(2) + "%");
 
-      // Pass progress to the API endpoint
-      // sendProgressToEndpoint(progress);
-    });
+    //   // Pass progress to the API endpoint
+    //   // sendProgressToEndpoint(progress);
+    // });
 
     videoStream.pipe(writeStream);
 
